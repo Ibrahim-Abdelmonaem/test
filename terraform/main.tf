0493c822ca136054ed645ffd751f202ca8bce616
Ibrahim-Abdelmonaem <ibrahim.abdelmonaem20@gmail.com>
2024-10-18 23:58:16 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,112 +1,44 @@
-terraform {
-    required_version = ">= 0.12"
-    backend "s3" {
-        bucket = "myapp-bucket"
-        key = "myapp/state.tfstate"
-        region = "eu-west-3"
-    }
+# terraform {
+# backend "s3" {
+#   bucket = "my-app581916614888"
+#   key = "myapp-state/Terraapp.tfstate"
+#   region = "us-east-1"
+
+# }
+
+# }
+
+provider "aws"{
+region = "us-east-1"
+access_key = AWS_ACCESS_KEY_ID
+secret_key = AWS_SECRET_ACCESS_KEY
 }
 
-provider "aws" {
-    region = var.region
+
+
+resource "aws_vpc" "my-vpc" {
+cidr_block = var.vpc_cidr_block
+tags = {
+    
+    Name = "${var.envi}-vpc"
+}
 }
 
-resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.vpc_cidr_block
-    tags = {
-        Name: "${var.env_prefix}-vpc"
-    }
+module "myapp-subnet" {
+
+    source = "./modules/subnet"
+    vpc_id = aws_vpc.my-vpc.id
+    subnet_cidr_block = var.subnet_cidr_block
+    avil_zone= var.avil_zone
+    envi = var.envi
 }
-
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name: "${var.env_prefix}-igw"
-    }
-}
-
-resource "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name: "${var.env_prefix}-main-rtb"
-    }
-}
-
-resource "aws_default_security_group" "default-sg" {
-    vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = "0.0.0.0/0"
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-
-    tags = {
-        Name: "${var.env_prefix}-default-sg"
-    }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
-    instance_type = var.instance_type
-
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-    availability_zone = var.avail_zone
-
-    associate_public_ip_address = true
-    key_name = "myapp-key-pair"
-
-    user_data = file("entry-script.sh")
-
-    tags = {
-        Name = "${var.env_prefix}-server"
-    }
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
+module "myapp-server"{
+source = "./modules/server"
+vpc_id = aws_vpc.my-vpc.id
+my_ip = var.my_ip
+envi = var.envi
+instance_type = var.instance_type
+subnet_id= module.myapp-subnet.subnet-details.id
+avil_zone= var.avil_zone
+public_key_location = var.public_key_location
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,8 @@
-variable vpc_cidr_block {
-    default = "10.0.0.0/16"
-}
-variable subnet_cidr_block {
-    default = "10.0.10.0/24"
-}
-variable avail_zone {
-    default = "eu-west-3a"
-}
-variable env_prefix {
-    default = "dev"
-}
-variable my_ip {
-    default = "41.237.84.73"
-}
-//variable jenkins_ip {
-//    default = "139.59.140.177/32"
-}
-variable instance_type {
-    default = "t2.micro"
-}
-variable region {
-    default = "eu-west-3"
-}
+variable "vpc_cidr_block" {}
+variable envi{}
+variable "subnet_cidr_block" {}
+variable "avil_zone"{}
+variable "my_ip"{}
+variable "instance_type"{}
+variable "public_key_location"{}
+variable "private_key_loc"{}
